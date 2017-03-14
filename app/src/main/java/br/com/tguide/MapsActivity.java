b77f86dd057909363de1b5d7a699156e9d59e3ec
Elvis Luciano <elvis.gs@gmail.com>
2017-03-14 20:38:22 -0300

    Primeiro commit

---
+++ app/src/main/java/br/com/tguide/MapsActivity.java
@@ -0,0 +1,119 @@
+package br.com.tguide;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnPoiClickListener;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.PointOfInterest;
+
+public class MapsActivity extends FragmentActivity implements ConnectionCallbacks, OnConnectionFailedListener, OnMapReadyCallback, OnPoiClickListener {
+
+    public static final int RATING_REQUEST = 1;
+    private GoogleMap mMap;
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLastLocation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(this, R.raw.map_styles));
+
+        LatLng myLocation = new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude());
+
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .zoom(18)
+                .tilt(30)
+                .target(myLocation)
+                .build();
+        mMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+        mMap.setOnPoiClickListener(this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onPoiClick(PointOfInterest poi) {
+        Intent intent = new Intent(getApplicationContext(), RatingActivity.class);
+        intent.putExtra("poi", poi);
+        startActivityForResult(intent, RATING_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RatingActivity.RATING_RESULT) {
+            PointOfInterest poi = data.getParcelableExtra("poi");
+            float rating = data.getFloatExtra("rating", 0f);
+
+            String message = "Local '" + poi.name + "' foi avaliado em " + rating;
+            Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+        }
+    }
+}
---
+++ app/src/main/java/br/com/tguide/RatingActivity.java
@@ -0,0 +1,45 @@
+package br.com.tguide;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.model.PointOfInterest;
+
+public class RatingActivity extends AppCompatActivity {
+
+    public static final int RATING_RESULT = 2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rating);
+
+        final Intent intent = getIntent();
+        final PointOfInterest poi = intent.getParcelableExtra("poi");
+        TextView title = (TextView) findViewById(R.id.title);
+        title.setText("Avalie o local \"" + poi.name + "\"");
+
+        Button doneButton = (Button) findViewById(R.id.done);
+        doneButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RatingBar ratingBar = (RatingBar) findViewById(R.id.rating);
+                TextView comments = (TextView) findViewById(R.id.comments);
+
+                Intent data = new Intent()
+                        .putExtra("poi", poi)
+                        .putExtra("rating", ratingBar.getRating())
+                        .putExtra("comments", comments.getText());
+
+                setResult(RATING_RESULT, data);
+                finish();
+            }
+        });
+    }
+}
