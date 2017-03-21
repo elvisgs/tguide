457c46b50318bbf50406fd5b349f80c680268beb
Elvis Luciano <elvis.gs@gmail.com>
2017-03-20 23:20:40 -0300

    Preparação para gerar apk release

--- app/src/main/java/br/com/tguide/service/SimpleCallback.java
+++ app/src/main/java/br/com/tguide/service/SimpleCallback.java
@@ -1,7 +1,5 @@
 package br.com.tguide.service;
 
-import android.util.Log;
-
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -10,11 +8,9 @@ public class SimpleCallback<T> implements Callback<T> {
 
     @Override
     public void onResponse(Call<T> call, Response<T> response) {
-        Log.i("API", call.request().url().toString() + " called without errors");
     }
 
     @Override
     public void onFailure(Call<T> call, Throwable t) {
-        Log.e("API", "Call to " + call.request().url() + " failed", t);
     }
 }
